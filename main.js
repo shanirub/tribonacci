3cb0d7b61b591ced4358921914c56ff017b57c40
shanirub <shani.rubovitch@gmail.com>
2019-03-02 18:36:51 +0100

    passed all tests

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 // signature : array[3] n : number of elements to show
 function tribonacci(signature,n){
-    //your code here
-
     if (n <= 3) {
-        for (let i = n; i > 0; i--) {
-            signature.pop
+        for (let i = 0; i < (3 - n); i++) {
+            signature.pop()
         }
     } else {
         for (let i = 2; i < n; i++) {
             signature.push(signature[i] + signature[i - 1] + signature[i - 2])
         }
-    }
-    
+    } 
     return signature
-    
-  }
+}
